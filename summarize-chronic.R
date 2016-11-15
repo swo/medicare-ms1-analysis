96bd5a800cea725661cdb9190c0fa1d7f7f9e6a3
swo <swo@mit.edu>
2016-11-15 15:08:27 -0500

    replaced summarize chronic

--- summarize-chronic.R
+++ summarize-chronic.R
@@ -2,12 +2,12 @@
 
 # Summarize the chronic conditions data into two-column table
 
-# Digits in the chronic condition data have a funny encoding:
-# "3" means that the beneficiary has the condition; other numbers
-# mean other things. Convert "3" to 1 and everything else to 0.
-change_digit = function(x) ifelse(x == 3, 1, 0)
-
 summarize_cc = function(in_fn, out_fn) {
+  # Digits in the chronic condition data have a funny encoding:
+  # "3" means that the beneficiary has the condition; other numbers
+  # mean other things. Convert "3" to 1 and everything else to 0.
+  change_digit = function(x) ifelse(x == 3, 1, 0)  
+  
   read_tsv(in_fn) %>%
     select(BENE_ID,
            AMI, ALZH, COPD, CHF, DIABETES, ISCHMCHT, STRKETIA,
