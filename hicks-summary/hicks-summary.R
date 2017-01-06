74f3c894bba4428b756a9a6b4d5d554d7f0d3716
swo <swo@mit.edu>
2017-01-06 16:56:27 -0500

    hicks: multiple years"

--- hicks-summary/hicks-summary.R
+++ hicks-summary/hicks-summary.R
@@ -35,61 +35,71 @@ summarize_by = function(dat, bene, by) {
 regions = read_tsv('../../db/census-regions/census-regions.tsv') %>%
   select(state, region)
 
-bene = read_tsv('../bene-2011.tsv') %>%
-  # keep only the beneficiaries that don't have bogus states
-  filter(!is.na(state)) %>%
-  # merge in the census region information
-  left_join(regions, by='state')
+analyze = function(year) {
+  bene_fn = sprintf('../bene-%s.tsv', year)
+  pde_fn = sprintf('../abx-pde-%s.tsv', year)
 
-# read in PDE data
-dat = read_tsv('../abx-pde-2011.tsv') %>%
-  # merge the abx class information
-  left_join(read_tsv('../abx-classes.tsv'), by='abx') %>%
-  # merge the beneficiary information, keeping only claims mapping to beneficiaries
-  # for which we have data (i.e., inner join)
-  inner_join(bene, by='bene')
+  bene = read_tsv(bene_fn) %>%
+    # keep only the beneficiaries that don't have bogus states
+    filter(!is.na(state)) %>%
+    # merge in the census region information
+    left_join(regions, by='state')
 
-# total prescriptions and prescriptions per 1000 people
-# broken down by antibiotic category
-n_beneficiaries = dim(bene)[1]
-rx_by_class = dat %>%
-  group_by(abx_class) %>%
-  summarize(n_rx=n()) %>%
-  mutate(rx_per_1k_ppl=n_rx*1000/n_beneficiaries)
-write_tsv(rx_by_class, 'rx_by_class.tsv')
+  # read in PDE data
+  dat = read_tsv(pde_fn) %>%
+    # merge the abx class information
+    left_join(read_tsv('../abx-classes.tsv'), by='abx') %>%
+    # merge the beneficiary information, keeping only claims mapping to beneficiaries
+    # for which we have data (i.e., inner join)
+    inner_join(bene, by='bene')
 
-# but with a separate list of the top 5 agents
-top_abx_rx = dat %>%
-  group_by(abx) %>%
-  summarize(n_rx=n()) %>%
-  arrange(desc(n_rx)) %>%
-  head(5) %>%
-  mutate(rx_per_1k_ppl=n_rx*1000/n_beneficiaries)
-write_tsv(top_abx_rx, 'top_abx_rx.tsv')
+  # total prescriptions and prescriptions per 1000 people
+  # broken down by antibiotic category
+  n_beneficiaries = dim(bene)[1]
+  rx_by_class = dat %>%
+    group_by(abx_class) %>%
+    summarize(n_rx=n()) %>%
+    mutate(rx_per_1k_ppl=n_rx*1000/n_beneficiaries)
+  write_tsv(rx_by_class, sprintf('rx_by_class-%s.tsv', year))
 
-# from here on, the denominators are taken from the beneficiary data grouped
-# in the same way as the consumption data, so we can use the summarize_by function
+  # but with a separate list of the top 5 agents
+  top_abx_rx = dat %>%
+    group_by(abx) %>%
+    summarize(n_rx=n()) %>%
+    arrange(desc(n_rx)) %>%
+    head(5) %>%
+    mutate(rx_per_1k_ppl=n_rx*1000/n_beneficiaries)
+  write_tsv(top_abx_rx, sprintf('top_abx_rx-%s.tsv', year))
 
-# then broken down by sex
-rx_by_sex = summarize_by(dat, bene, 'sex')
-write_tsv(rx_by_sex, 'rx_by_sex.tsv')
+  # from here on, the denominators are taken from the beneficiary data grouped
+  # in the same way as the consumption data, so we can use the summarize_by function
 
-# then by age group
-# (the input to summarize_by is modified, since we want to group by age *group*,
-# not just by raw age)
-age_group_f = function(x) bounds_group(x, list(c(0, 65), c(66, 75), c(76, 85), c(86, Inf)))
-rx_by_age = summarize_by(dat %>% mutate(age_group=age_group_f(age)), bene %>% mutate(age_group=age_group_f(age)), 'age_group')
-write_tsv(rx_by_age, 'rx_by_age.tsv')
+  # then broken down by sex
+  rx_by_sex = summarize_by(dat, bene, 'sex')
+  write_tsv(rx_by_sex, sprintf('rx_by_sex-%s.tsv', year))
 
-# then by census region
-rx_by_region = summarize_by(dat, bene, 'region')
-write_tsv(rx_by_region, 'rx_by_region.tsv')
+  # then by age group
+  # (the input to summarize_by is modified, since we want to group by age *group*,
+  # not just by raw age)
+  age_group_f = function(x) bounds_group(x, list(c(0, 65), c(66, 75), c(76, 85), c(86, Inf)))
+  rx_by_age = summarize_by(dat %>% mutate(age_group=age_group_f(age)), bene %>% mutate(age_group=age_group_f(age)), 'age_group')
+  write_tsv(rx_by_age, sprintf('rx_by_age-%s.tsv', year))
 
-# prescriptions per 1000 people by state
-# (broken into sextiles to make a colored map)
-rx_by_state = summarize_by(dat, bene, 'state') %>%
-  mutate(sextile=ntile(rx_per_1k_ppl, 6))
-write_tsv(rx_by_state, 'rx_by_state.tsv')
+  # then by census region
+  rx_by_region = summarize_by(dat, bene, 'region')
+  write_tsv(rx_by_region, sprintf('rx_by_region-%s.tsv', year))
+
+  # prescriptions per 1000 people by state
+  # (broken into sextiles to make a colored map)
+  rx_by_state = summarize_by(dat, bene, 'state') %>%
+    mutate(sextile=ntile(rx_per_1k_ppl, 6))
+  write_tsv(rx_by_state, sprintf('rx_by_state-%s.tsv', year))
+}
+
+analyze('2011')
+analyze('2012')
+analyze('2013')
+analyze('2014')
 
 # logistic regression on...
 # dependent: county in top quartile for prescription rate
