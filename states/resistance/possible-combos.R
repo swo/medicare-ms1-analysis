79664e783f06e4e2a72cfa92809eea5894decbd2
swo <swo@picus>
2017-01-27 13:46:47 -0500

    merging in old code

--- states/resistance/possible-combos.R
+++ states/resistance/possible-combos.R
@@ -25,10 +25,64 @@ add_organisms = function(x) {
 
 # get all combos of cons_res with each bug
 # keep only those res-drug/bug combos are geographically coherent
-dat = cons_res %>%
+coherent_bug_drug = cons_res %>%
   rowwise %>%
   do(add_organisms(.)) %>%
+  ungroup %>%
   left_join(coherence, by=c('resistance_drug', 'bug')) %>%
-  filter(level %in% c('state', 'hrr', 'zipcode'))
+  mutate(state_coherent=level %in% c('state', 'hrr', 'zipcode'))
 
-# do the correlations 
+# do the correlations
+
+abg = read_tsv('../../../../antibiogram/data/abg.tsv') %>%
+  mutate(drug=tolower(drug)) %>%
+  rename(resistance_drug=drug)
+
+known_states = abg %$% state %>% unique %>% sort
+
+state_bene = read_tsv('../bene-consumption/state-bene-2011.tsv') %>%
+  filter(state %in% known_states)
+state_cons = read_tsv('../bene-consumption/state-consumption-2011.tsv') %>%
+  filter(state %in% known_states)
+
+sus = coherent_bug_drug %>%
+  left_join(abg, by=c('bug', 'resistance_drug')) %>%
+  group_by(bug, resistance_drug, state) %>%
+  summarize(n_abg=n(),
+            n_isolates=sum(n_isolates, na.rm=TRUE),
+            n_years=length(unique(year)),
+            median_sus=median(percent_susceptible, na.rm=TRUE),
+            mean_sus=mean(percent_susceptible, na.rm=TRUE),
+            sd_sus=sd(percent_susceptible, na.rm=TRUE),
+            min_sus=min(percent_susceptible, na.rm=TRUE),
+            max_sus=max(percent_susceptible, na.rm=TRUE))
+
+write_tsv(sus, 'tmp-sus')
+
+models = function(df) {
+  out = data_frame()
+  for (resp in c('median_sus', 'mean_sus', 'min_sus', 'max_sus')) {
+    for (expl in c('claims_per_1k_ppl', 'did')) {
+      sprintf("%s %s %s\n", resp, expl, head(df$resistance_drug, 1), head(df$bug, 1)) %>% cat
+      new_rows = cor.test(df[[resp]], df[[expl]], method='spearman') %>%
+      #new_rows = df %>%
+        #mblm(as.formula(paste0(resp, '~', expl)), dataframe=.) %>%
+        tidy %>%
+        mutate(response=resp, explanatory=expl)
+      out = bind_rows(out, new_rows)
+    }
+  }
+  out
+}
+
+x = state_cons %>%
+  ungroup %>%
+  left_join(sus, by=c('consumption_drug', 'state')) %>%
+  filter(!is.na(n_isolates)) %>%
+  arrange(drug)
+
+y = x %>%
+  group_by(bug, drug) %>%
+  do(models(.))
+
+write_tsv(y, 'tmp-res')
