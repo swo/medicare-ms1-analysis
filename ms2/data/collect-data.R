ea076a37692611babde2ad44ddc29713e3b20c3d
swo <swo@alum.mit.edu>
2017-05-03 13:11:21 -0400

    modernizing ms2
    - Use states mainly (HRRs are more noisy)
    - Use a short-list of bug/drug candidates
    - Collect data from all years

--- ms2/data/collect-data.R
+++ ms2/data/collect-data.R
@@ -19,68 +19,81 @@ consumption_groups = read_tsv('../consumption_groups.tsv')
 regions = read_tsv('../../../db/census-regions/census-regions.tsv') %>%
   select(state, state_abbreviation, region)
 
-hrr = read_tsv('../../../db/hrr/hrr.tsv') %>%
-  filter(year==2011) %>%
-  select(zipcode, hrr)
+hrr_all = read_tsv('../../../db/hrr/hrr.tsv')
 
-bene = read_tsv('../../bene_2011.tsv') %>%
-  filter(between(age, 66, 100), hmo_months==0) %>%
-  left_join(regions, by='state') %>%
-  left_join(hrr, by='zipcode')
+summarize_inequality = function(year) {
+  hrr = hrr_all %>%
+    filter(year==year) %>%
+    select(zipcode, hrr)
 
-pde = read_tsv('../../pde_2011.tsv') %>%
-  rename(drug=antibiotic) %>%
-  left_join(consumption_groups, by='drug') %>%
-  group_by(bene_id, drug_group) %>%
-  summarize(n_claims=n()) %>%
-  ungroup
+  bene_fn = sprintf('../../bene_%i.tsv', year)
+  pde_fn = sprintf('../../pde_%i.tsv', year)
+  state_all_fn = sprintf('state_ineq_all_%i.tsv', year)
+  state_fn = sprintf('state_ineq_%i.tsv', year)
+  hrr_all_fn = sprintf('hrr_all_%i.tsv', year)
+  hrr_fn = sprintf('hrr_ineq_%i.tsv', year)
 
-# compute total claims separately from individual drug groups
-total_pde = pde %>%
-  group_by(bene_id) %>%
-  summarize(n_claims=sum(n_claims)) %>%
-  right_join(bene, by='bene_id') %>%
-  replace_na(list(n_claims=0)) %>%
-  group_by(state) %>%
-  do(inequalities(.$n_claims)) %>%
-  ungroup() %>%
-  mutate(mean_fnz_residual=lm_residuals(mean, fnz))
+  bene = read_tsv(bene_fn) %>%
+    filter(between(age, 66, 96), hmo_months==0) %>%
+    left_join(regions, by='state') %>%
+    left_join(hrr, by='zipcode')
 
-write_tsv(total_pde, 'all_drug_ineq.tsv')
+  pde = read_tsv(pde_fn) %>%
+    rename(drug=antibiotic) %>%
+    left_join(consumption_groups, by='drug') %>%
+    group_by(bene_id, drug_group) %>%
+    summarize(n_claims=n()) %>%
+    ungroup
 
-state_denom = bene %>% count(state) %>% rename(n_bene=n)
-state_inequality = pde %>%
-  right_join(select(bene, bene_id, state), by='bene_id') %>%
-  filter(!is.na(drug_group)) %>%
-  left_join(state_denom, by='state') %>%
-  group_by(state, drug_group) %>%
-  do(inequalities(pad0(.$n_claims, unique(.$n_bene)))) %>%
-  ungroup() %>%
-  mutate(mean_fnz_residual=lm_residuals(mean, fnz))
+  # compute total claims separately from individual drug groups
+  total_pde = pde %>%
+    group_by(bene_id) %>%
+    summarize(n_claims=sum(n_claims)) %>%
+    right_join(bene, by='bene_id') %>%
+    replace_na(list(n_claims=0)) %>%
+    group_by(state) %>%
+    do(inequalities(.$n_claims)) %>%
+    ungroup() %>%
+    mutate(mean_fnz_residual=lm_residuals(mean, fnz))
 
-write_tsv(state_inequality, 'drug_ineq.tsv')
+  write_tsv(total_pde, state_all_fn)
 
-# repeat this for the HRRs
-hrr_total_pde = pde %>%
-  group_by(bene_id) %>%
-  summarize(n_claims=sum(n_claims)) %>%
-  right_join(bene, by='bene_id') %>%
-  replace_na(list(n_claims=0)) %>%
-  group_by(hrr) %>%
-  do(inequalities(.$n_claims)) %>%
-  ungroup() %>%
-  mutate(mean_fnz_residual=lm_residuals(mean, fnz))
+  state_denom = bene %>% count(state) %>% rename(n_bene=n)
+  state_inequality = pde %>%
+    right_join(select(bene, bene_id, state), by='bene_id') %>%
+    filter(!is.na(drug_group)) %>%
+    left_join(state_denom, by='state') %>%
+    group_by(state, drug_group) %>%
+    do(inequalities(pad0(.$n_claims, unique(.$n_bene)))) %>%
+    ungroup() %>%
+    mutate(mean_fnz_residual=lm_residuals(mean, fnz))
 
-write_tsv(hrr_total_pde, 'hrr_all_drug_ineq.tsv')
+  write_tsv(state_inequality, state_fn)
 
-hrr_denom = bene %>% count(hrr) %>% rename(n_bene=n)
-hrr_inequality = pde %>%
-  right_join(select(bene, bene_id, hrr), by='bene_id') %>%
-  filter(!is.na(drug_group)) %>%
-  left_join(hrr_denom, by='hrr') %>%
-  group_by(hrr, drug_group) %>%
-  do(inequalities(pad0(.$n_claims, unique(.$n_bene)))) %>%
-  ungroup() %>%
-  mutate(mean_fnz_residual=lm_residuals(mean, fnz))
+  # repeat this for the HRRs
+  hrr_total_pde = pde %>%
+    group_by(bene_id) %>%
+    summarize(n_claims=sum(n_claims)) %>%
+    right_join(bene, by='bene_id') %>%
+    replace_na(list(n_claims=0)) %>%
+    group_by(hrr) %>%
+    do(inequalities(.$n_claims)) %>%
+    ungroup() %>%
+    mutate(mean_fnz_residual=lm_residuals(mean, fnz))
 
-write_tsv(hrr_inequality, 'hrr_drug_ineq.tsv')
+  write_tsv(hrr_total_pde, hrr_all_fn)
+
+  hrr_denom = bene %>% count(hrr) %>% rename(n_bene=n)
+  hrr_inequality = pde %>%
+    right_join(select(bene, bene_id, hrr), by='bene_id') %>%
+    filter(!is.na(drug_group)) %>%
+    left_join(hrr_denom, by='hrr') %>%
+    group_by(hrr, drug_group) %>%
+    do(inequalities(pad0(.$n_claims, unique(.$n_bene)))) %>%
+    ungroup() %>%
+    mutate(mean_fnz_residual=lm_residuals(mean, fnz))
+
+  write_tsv(hrr_inequality, hrr_fn)
+}
+
+for (y in 2011:2014) summarize_inequality(y)
