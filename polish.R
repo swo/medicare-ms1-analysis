f162727668a234e5383b6918f7f937984f77e4e2
swo <swo@alum.mit.edu>
2017-08-24 23:33:51 -0400

    consolidated polish

--- polish.R
+++ polish.R
@@ -3,6 +3,7 @@
 abx_class = read_tsv('abx_class.tsv')
 
 polish_year = function(y) {
+  # bene
   bene_in_fn = sprintf('../data/bene_%i.tsv', y)
   bene_out_fn = sprintf('bene_%i.tsv', y)
 
@@ -10,6 +11,7 @@ polish_year = function(y) {
     select(bene_id=BENE_ID, state, sex, race, zipcode, age=AGE, buyin_months=BUYIN_MO) %>%
     write_tsv(bene_out_fn)
 
+  # pde
   pde_in_fn = sprintf('../data/pde_%i.tsv', y)
   pde_out_fn = sprintf('pde_%i.tsv', y)
 
@@ -20,6 +22,7 @@ polish_year = function(y) {
     select(-generic_name) %>%
     write_tsv(pde_out_fn)
 
+  # diagnoses: carrier and outpatient
   car_in_fn = sprintf('../data/car_%i.tsv', y)
   op_in_fn = sprintf('../data/op_%i.tsv', y)
   dx_out_fn = sprintf('dx_%i.tsv', y)
@@ -31,6 +34,14 @@ polish_year = function(y) {
     mutate(dx_date=dmy(dx_date)) %>%
     distinct() %>%
     write_tsv(dx_out_fn)
+
+  # chronic conditions
+  cc_fn = sprintf('../data/cc_%i.tsv', y)
+  cc_out_fn = sprintf('cc_%i.feather', y)
+  read_tsv(cc_fn) %>%
+    rename(bene_id=BENE_ID) %>%
+    mutate_at(vars(AMI:HYPOTH), function(x) x==3) %>%
+    write_feather(cc_out_fn)
 }
 
 library(parallel)
