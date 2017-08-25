2dbf9f2d5a4103c7fb048a948f9f2acecb134f34
swo <swo@alum.mit.edu>
2017-08-24 23:22:37 -0400

    faster polish

--- polish-bene-pde.R
+++ polish-bene-pde.R
@@ -1,50 +1,41 @@
 #!/usr/bin/env Rscript
 
 abx_class = read_tsv('abx_class.tsv')
-dx_class = read_tsv('../data/fd_codes.tsv') %>% select(diagnosis=code, diagnosis_type)
-
-polish_bene = function(df) {
-  rename(df, bene_id=BENE_ID, age=AGE, buyin_months=BUYIN_MO)
-}
-
-polish_pde = function(df) {
-  rename(df, bene_id=BENE_ID, service_date=SRVC_DT, days_supply=DAYSSPLY, generic_name=GNN) %>%
-    mutate(service_date=dmy(service_date)) %>%
-    left_join(abx_class, by='generic_name') %>%
-    select(-generic_name)
-}
-
-polish_dx = function(df) {
-  rename(df, bene_id=BENE_ID) %>%
-    mutate(from_date=dmy(from_date)) %>%
-    group_by(bene_id, diagnosis, from_date) %>%
-    summarize(n_encounter_claim=n()) %>%
-    ungroup() %>%
-    left_join(dx_class, by='diagnosis')
-}
 
 polish_year = function(y) {
-  pde_in_fn = sprintf('../data/pde_%i.tsv', y)
-  pde_out_fn = sprintf('pde_%i.tsv', y)
-
-  read_tsv(pde_in_fn) %>%
-    polish_pde() %>%
-    write_tsv(pde_out_fn)
-
   bene_in_fn = sprintf('../data/bene_%i.tsv', y)
   bene_out_fn = sprintf('bene_%i.tsv', y)
 
   read_tsv(bene_in_fn, col_types=cols(zipcode='c')) %>%
-    polish_bene() %>%
+    select(bene_id=BENE_ID, state, sex, race, zipcode, age=AGE, buyin_months=BUYIN_MO) %>%
     write_tsv(bene_out_fn)
 
-  car_in_fn = sprintf('../data/fd_dx_car_claims_%i.tsv', y)
-  op_in_fn = sprintf('../data/fd_dx_op_claims_%i.tsv', y)
+  pde_in_fn = sprintf('../data/pde_%i.tsv', y)
+  pde_out_fn = sprintf('pde_%i.tsv', y)
+
+  read_tsv(pde_in_fn) %>%
+    select(bene_id=BENE_ID, pde_id=PDE_ID, pde_date=SRVC_DT, days_supply=DAYSSPLY, gcdf=GCDF, generic_name=GNN) %>%
+    mutate(pde_date=dmy(pde_date)) %>%
+    left_join(abx_class, by='generic_name') %>%
+    select(-generic_name) %>%
+    write_tsv(pde_out_fn)
+
+  car_in_fn = sprintf('../data/car_%i.tsv', y)
+  op_in_fn = sprintf('../data/op_%i.tsv', y)
   dx_out_fn = sprintf('dx_%i.tsv', y)
 
-  bind_rows(read_tsv(car_in_fn), read_tsv(op_in_fn)) %>%
-    polish_dx() %>%
+  bind_rows(
+    read_tsv(car_in_fn) %>% select(bene_id=BENE_ID, claim_id=CLM_ID, dx_date=EXPNSDT1, dx_code=LINE_ICD_DGNS_CD, dx_category=diagnosis_category),
+    read_tsv(op_in_fn) %>% select(bene_id=BENE_ID, claim_id=CLM_ID, dx_date=THRU_DT, dx_code=PRNCPAL_DGNS_CD, dx_category=diagnosis_category)
+  ) %>%
+    mutate(dx_date=dmy(dx_date)) %>%
+    distinct() %>%
     write_tsv(dx_out_fn)
 }
 
-for (year in 2011:2014) polish_year(year)
+library(parallel)
+cluster = makeCluster(3, type='FORK')
+
+parLapply(cluster, 2011:2014, polish_year)
+
+stopCluster(cluster)
