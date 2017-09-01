badb6f9bb1f3635ad25f466d22f062ec672549b9
swo <swo@alum.mit.edu>
2017-09-01 10:51:59 -0400

    adapting

--- ms1/ms1_3dx.R
+++ ms1/ms1_3dx.R
@@ -12,7 +12,6 @@ bene = read_tsv('data/bene.tsv') %>%
          race=factor(race, levels=c('white', 'black', 'Hispanic', 'other'))) %>%
   mutate(age=age-1)
 
-# trends in diagnoses by beneficiary
 read_years = function(years, template) {
   lapply(years, function(y) {
     read_tsv(sprintf(template, y)) %>%
@@ -21,14 +20,23 @@ read_years = function(years, template) {
     bind_rows()
 }
 
+# trends in diagnoses by beneficiary
 dx_by_bene = read_years(2011:2014, '../../data/dx_by_bene_%i.tsv') %>%
   rename(bene_id=BENE_ID, dx_cat=diagnosis_category)
 
 dxs = unique(dx_by_bene$dx_cat)
 
+# first, a table
+dx_trends_table = dx_by_bene %>%
+  group_by(year, dx_cat) %>%
+  summarize(n_dx=sum(n_dx)) %>%
+  ungroup() %T>%
+  output_table('dx_trends_table')
+
 glm_f = function(df, frmla, ...) eval(substitute(function(df2) glm(frmla, data=df2, ...)))(df)
 frmla = y ~ year + age + n_cc + sex + race + dual + region
 
+# then models
 dx_trends_models = lapply(dxs, function(dx) {
   dx_by_bene %>%
     filter(dx_cat==dx) %>%
@@ -63,7 +71,7 @@ dx_from_pde_table = crossing(antibiotic=top_abx, dx_cat=dxs) %>%
     # how what fraction of PDEs for this abx had this dx upstream?
     pde_denom %>%
       filter(antibiotic==a) %>%
-      left_join(filter(dx_from_pde, dx_cat==dx), by='pde_id') %>%
+      left_join(filter(dx_from_pde, antibiotic==a, dx_cat==dx), by='pde_id') %>%
       mutate(present=!is.na(dx_cat)) %>%
       count(present)
   })(.$antibiotic, .$dx_cat)) %T>%
@@ -73,26 +81,28 @@ dx_from_pde_table = crossing(antibiotic=top_abx, dx_cat=dxs) %>%
 # swo: test this part
 dx_to_pde = read_years(2011:2014, '../../data/dx_to_pde_%i.tsv') %>%
   rename(bene_id=BENE_ID, dx_cat=diagnosis_category) %>%
-  mutate(dummy=TRUE) %>% spread(antibiotic, dummy) %>%
-  replace_na(as.list(rep(0, length(top_abx))), top_abx) %>%
-  left_join(bene, by=c('year', 'bene_id'))
+  replace_na(list(antibiotic='no_abx')) %>%
+  # drop no infection and no abx, since we won't look at those
+  filter(!(dx_cat=='not_infectious' & antibiotic=='no_abx')) %>%
+  # lump abx outside of top 10 into "other"
+  mutate(antibiotic=fct_other(factor(antibiotic), keep=c(top_abx, 'no_abx'))) %>%
+  count(year, bene_id, dx_cat, antibiotic)
 
-dx_rx_table = crossing(antibiotic=top_abx, dx_cat=dxs) %>%
-  group_by(antibiotic, dx_cat) %>%
-  do((function(a, dx) {
-    dx_to_pde %>%
-      filter(dx_cat==dx) %>%
-      count(!!sym(a))
-  })(.$antibiotic, .$dx_cat)) %T>%
+dx_rx_table = dx_to_pde %>%
+  group_by(year, dx_cat, antibiotic) %>%
+  summarize(n=sum(n)) %>%
+  mutate(f=n/sum(n)) %>% 
+  ungroup() %T>%
   output_table('dx_rx_table')
 
+frmla = y ~ year + age + n_cc + sex + race + dual + region
 dx_rx_trends = crossing(antibiotic=top_abx, dx_cat=dxs) %>%
   group_by(antibiotic, dx_cat) %>%
   do((function(a, dx) {
-    frmla = as.formula(str_interp("${a} ~ year + age + n_cc + sex + race + dual + region"))
     dx_to_pde %>%
       filter(dx_cat==dx) %>%
-      glm_f(frmla, family='binomial') %>%
+      mutate(y=antibiotic==a) %>%
+      glm_f(frmla, family='binomial', weights=n) %>%
       tidy
-  }) %T>%
+  })(.$antibiotic, .$dx_cat)) %T>%
   output_table('dx_rx_trends')
