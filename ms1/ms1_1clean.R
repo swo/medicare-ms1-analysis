047a9346c07cbde829124cfc1b52958f87ff6bb1
swo <swo@alum.mit.edu>
2017-10-21 16:46:59 -0400

    one-step polish

--- ms1/ms1_1clean.R
+++ ms1/ms1_1clean.R
@@ -1,26 +1,28 @@
 #!/usr/bin/env Rscript
 
-# Process the bene, pde, and dx data
-# so it's ready for analysis
+# Process the bene, pde, and dx data so it's ready for analysis
 
-# Keep chronic conditions that have a single reference year. Exclude Alzheimer's,
-# which is a subset of Alz. & dementia
-condition_names = read_tsv('../chronic-conditions/names.tsv') %>% filter(condition_code != 'ALZH')
-names_1yr_ccs = condition_names %>% filter(condition_ref_years==1) %$% condition_code
-sum_1yr_cc = paste0(names_1yr_ccs, collapse=' + ')
+# lower the names of a tibble
+lower_names = function(df) {
+  ns = names(df)
+  rename(df, !!!setNames(as.list(ns), sapply(ns, tolower)))
+}
 
 regions = read_tsv('../../db/census-regions/census-regions.tsv') %>%
   select(state, region)
 
 load_data = function(year) {
   # pde data
-  pde = read_tsv(sprintf('../pde_%i.tsv', year)) %>%
-    select(bene_id, pde_date, antibiotic, fill_num) %>%
-    # for each bene, date, and drug, keep only one record. give preference to first fills.
+  pde = read_tsv(sprintf('../../data/pde_%i.tsv', year)) %>%
+    lower_names() %>%
+    mutate(pde_date=dmy(pde_date)) %>%
+    select(bene_id, pde_id, pde_date, antibiotic, fill_num) %>%
+    # for each bene, date, and drug, keep only one record, giving preference to first fills.
     group_by(bene_id, pde_date, antibiotic) %>%
-    summarize(fill_num=min(fill_num)) %>%
+    summarize(pde_id=min(pde_id), fill_num=min(fill_num)) %>%
     ungroup() %>%
-    mutate(year=year, pde_id=1:n())
+    mutate(year=year) %>%
+    select(year, bene_id, pde_id, antibiotic, fill_num)
 
   # total numbers of PDEs
   n_claims = count(pde, bene_id) %>% rename(n_claims=n)
@@ -29,21 +31,15 @@ load_data = function(year) {
     count(bene_id) %>%
     rename(n_claims_firstfill=n)
 
-  # count chronic conditions. 'n_cc' means one-year cc's
-  cc = read_feather(sprintf('../cc_%i.feather', year)) %>%
-    mutate_(n_cc=sum_1yr_cc) %>%
-    ungroup() %>%
-    select(bene_id, n_cc)
-
-  # join the cc, summary PDE, and dx data into bene
-  bene = read_tsv(sprintf('../bene_%i.tsv', year)) %>%
-    mutate(year=year) %>%
+  # join the summary PDE
+  bene = read_tsv(sprintf('../../data/bene_%i.tsv', year)) %>%
+    lower_names() %>%
     left_join(regions, by='state') %>%
-    left_join(cc, by='bene_id') %>%
     left_join(n_claims, by='bene_id') %>%
     left_join(n_claims_firstfill, by='bene_id') %>%
     replace_na(list(n_claims=0, n_claims_firstfill=0)) %>%
-    select(bene_id, year, age, sex, race, dual, n_cc, region, n_claims, n_claims_firstfill)
+    mutate(year=year, dual=buyin_mo>0) %>%
+    select(year, bene_id, age, sex, race, dual, n_cc, region, n_claims, n_claims_firstfill)
 
   # take the bene, pde (with diagnoses) and diagnoses (separately)
   list(bene=bene, pde=pde)
